9829a0110d32e160bccd345aba9d9c02f4b7b48c
Andrey <calwe89@gmail.com>
2022-08-13 11:04:18 +1000

    041A+042A

---
+++ 041A/Program.cs
@@ -0,0 +1,51 @@
+﻿//Task_41A
+// Дана квадратная матрица. Проверить, является ли она симметричной относительно главной диагонали.
+
+//создаем и заполняем двумерный массив
+int[,] a = new int[4, 4];
+FillArray (a);
+//Выводим массив на экран
+PrintArray (a);
+//проверяем на симметричность
+CheckSymmetry (a);
+
+//методы
+void FillArray(int[,]a)
+{
+Random random=new Random();
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            a[i,j]= random.Next(0,2);
+        }
+    }
+}
+
+void PrintArray(int[,] a)
+{
+    for(int i=0;i<a.GetLength(0);i++)
+    {
+        for(int j=0;j<a.GetLength(1);j++) System.Console.Write($"{a[i,j]} ");
+            System.Console.WriteLine();
+    }
+}
+
+void CheckSymmetry(int[,] a)
+{
+    bool symmetry = true;
+        for(int i=0;i<a.GetLength(0);i++)
+            {
+                for(int j=0;j<a.GetLength(1);j++) 
+                {
+                    if (a[i, j] != a[j, i])
+                    { 
+                        symmetry = false;
+                    }
+                }
+            }
+    if (symmetry) 
+        Console.WriteLine("Созданная матрица симметрична");
+    else
+        Console.WriteLine("Созданная матрица не симметрична"); 
+}
---
+++ 042A/Program.cs
@@ -0,0 +1,75 @@
+﻿//Task_42A
+/*
+Даны две квадратных таблицы чисел. Требуется построить третью,каждый элемент которой равен сумме элементов,
+стоящих на том же месте в 1-й и 2-й таблицах.
+Входные данные:
+Во входном файле записано сначала число N, затем записана первая таблица,
+а после нее - вторая. Элементы таблиц - числа от 0 до 100.
+100>=N>=1.
+*/
+
+// создаем матрицу
+int N;
+int M;
+Console.Write("Введите размер квадратной матрицы и нажмите клавишу Enter: ");
+string? s=Console.ReadLine();
+N=M=Convert.ToInt32(s);
+Console.WriteLine();
+//инициализируем и заполняем первую матрицу случайными числами
+int [,] a1 = new int[N,M];
+FillArray (a1);
+Console.WriteLine ("Первая матрица: ");
+PrintArray (a1);
+Console.WriteLine();
+//инициализируем и заполняем вторую матрицу случайными числами
+int [,] a2 = new int[N,M];
+FillArray (a2);
+Console.WriteLine ("Вторая матрица: ");
+PrintArray (a2);
+Console.WriteLine();
+//инициализируем и строим третью матрицу
+int [,] a3 = new int[N,M];
+SumArray12 (a3);
+Console.WriteLine ("Третья матрица: ");
+PrintArray (a3);
+Console.WriteLine();
+
+
+
+//Заполнение массива случайными числами
+void FillArray(int[,]a)
+{
+Random random=new Random();
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            a[i,j]= random.Next(1,101);
+        }
+    }
+}
+
+//Вывод массива на экран
+void PrintArray(int[,]a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            System.Console.Write($"{a[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+//суммируем элементы первой и второй матрицы в третью
+void SumArray12(int[,]a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+        {
+            a[i,j]=a1[i,j]+a2[i,j];
+        }
+    }
+}
